d661c56f229fa378a57acedcff4f855672aec9db
tshepoandi <lovekilledtshepo@gmail.com>
2024-09-03 08:33:44 +0200

    created dbContext

---
+++ Data/BackendDbContext.cs
@@ -0,0 +1,14 @@
+using Microsoft.EntityFrameworkCore;
+using backends.Entities;
+
+namespace backends.Data;
+{
+    public class BackendsDbContext : DbContext
+    {
+        public BackendsDbContext(DbContextOptions<BackendsDbContext> options) : base(options)
+        {
+
+        }
+        public DbSet<User> Users {get; set;}
+    }
+}
--- Entities/User.cs
+++ Entities/User.cs
@@ -2,12 +2,10 @@ namespace backends.Entities
 {
     public class User
     {
-        public int Id { get; set; }
+        public Guid Id { get; set; }
 
-        [Required]
-        [StringLength(50)]
-        public string Username { get; set; }
-        // public byte[] PasswordHash { get; set; }
-        // public byte[] PasswordSalt { get; set; }
+        public string Name { get; set; }
+
+        public string Email { get; set; }
     }
 }
