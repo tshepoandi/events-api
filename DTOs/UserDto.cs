ad3c6fa0d37256bd08b78774cb29cdd499f9a323
tshepoandi <lovekilledtshepo@gmail.com>
2024-09-18 14:06:54 +0200

    got shit done

--- DTOs/UserDto.cs
+++ DTOs/UserDto.cs
@@ -6,10 +6,9 @@ public class UserCreateDto
     [StringLength(50)]
     public string Username { get; set; }
 
-    [Required]
-    [StringLength(100)]
-    public string Password { get; set; }
-
+    // [Required]
+    // [StringLength(100)]
+    // public string Password { get; set; }
     [Required]
     [EmailAddress]
     [StringLength(100)]
