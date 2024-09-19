4548acff9ba26b1a4a33398f7fc5be41a8581c34
tshepoandi <lovekilledtshepo@gmail.com>
2024-09-19 08:00:14 +0200

    added is attending event controller to the database

--- Controllers/EventsControllers.cs
+++ Controllers/EventsControllers.cs
@@ -115,6 +115,46 @@ namespace backends.Controllers
             return NoContent();
         }
 
+        [HttpPost("{id}/attend")]
+        public async Task<IActionResult> AttendEvent(int id, int userId)
+        {
+            var @event = await _context.Events.FindAsync(id);
+            var user = await _context.Users.FindAsync(userId);
+
+            if (@event == null || user == null)
+            {
+                return NotFound();
+            }
+
+            var attendance =
+                new EventAttendee { UserId = userId, EventId = id };
+
+            _context.EventAttendees.Add (attendance);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        [HttpDelete("{id}/attend")]
+        public async Task<IActionResult> UnattendEvent(int id, int userId)
+        {
+            var attendance =
+                await _context
+                    .EventAttendees
+                    .FirstOrDefaultAsync(ea =>
+                        ea.EventId == id && ea.UserId == userId);
+
+            if (attendance == null)
+            {
+                return NotFound();
+            }
+
+            _context.EventAttendees.Remove (attendance);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
         private bool EventExists(int id)
         {
             return _context.Events.Any(e => e.Id == id);
