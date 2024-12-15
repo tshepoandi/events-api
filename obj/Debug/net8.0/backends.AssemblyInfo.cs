a8634ffbafc305b105158a7d0a3b7a3553ed4d86
tshepoandi <lovekilledtshepo@gmail.com>
2024-12-15 18:42:49 +0200

    connect to live postgres database

--- obj/Debug/net8.0/backends.AssemblyInfo.cs
+++ obj/Debug/net8.0/backends.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("backends")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+592794cfe4e89de92c7a63f3d2ded9e44d6e5b18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3c5decf232f8c918934232d3e95ca8a0ca13595f")]
 [assembly: System.Reflection.AssemblyProductAttribute("backends")]
 [assembly: System.Reflection.AssemblyTitleAttribute("backends")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
